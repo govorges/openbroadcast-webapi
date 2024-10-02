0507fd275d87e837f37e4e89562592bbf9ec360e
gianaut <gio@gov.org.es>
2024-10-02 04:19:08 -0400

    Rewrite VideoAPI.uploads__Create() to parse JSON response object

--- services/Video.py
+++ services/Video.py
@@ -1,5 +1,6 @@
 import requests
 from os import environ
+import json
 
 class VideoAPI:
     def __init__(self):
@@ -7,13 +8,20 @@ class VideoAPI:
         if "http" not in API_Endpoint_Address:
             self.API_Endpoint_URL = f"http://{API_Endpoint_Address}"
 
-    def uploads__Create(self, id: str, video_metadata: dict):
+    def uploads__Create(self, id: str, video_metadata: dict) -> dict | None:
         requestURL = f"{self.API_Endpoint_URL}/uploads/create"
-        headers = { "id": id }
+        headers = { 
+            "id": id 
+        }
         
         r = requests.post(requestURL, headers=headers, json=video_metadata)
+        try:
+            request_return_data = r.json()
+        except json.JSONDecodeError:
+            request_return_data = {}
+        object_data = request_return_data.get("object_data")
 
-        return r.json()
+        return object_data 
 
     def videos__GenerateID(self):
         requestURL = f"{self.API_Endpoint_URL}/videos/generate_id"
